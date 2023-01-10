7a6895ddeda6b27d3177f7bac558cda1518c3c79
Zeeroon-e <chia.kmohamad@hotmail.com>
2023-01-10 11:45:24 +0100

    data shows correct

--- pdf-report/src/App.tsx
+++ pdf-report/src/App.tsx
@@ -1,22 +1,20 @@
 import './App.css'
 import "chart.js/auto";
-import data from "./data/data.json";
-import { Bar } from "react-chartjs-2";
+import { avrgScoreDiagramData } from "./data/dataexport";
+import { Bar, Pie } from "react-chartjs-2";
 
 function App() {
  
-    let avrg: String;
-const newData = Object.keys(data)
-
-  const avrgData: string[] = newData.map((object) => object);
-console.log(avrgData);
+  console.log(avrgScoreDiagramData);
+  
+  
 
 
   return (
     <div className="wrapper">
       <h1>pdf report</h1>
       <p>intro/info</p>
-
+      <Bar data={avrgScoreDiagramData} />
 
  
     </div>
--- pdf-report/src/data/dataexport.ts
+++ pdf-report/src/data/dataexport.ts
@@ -1,16 +1,54 @@
 import data from "./data.json"
 
-
+const newData = data.data.courseDashboard.modules;
 
 
 //avrg score
 
-function avrgScore() {
-    let avrg: String;
-Object.keys(data)
-console.log(data);
+const avrgData: any = newData.map((object) => object.goals);
 
-    const avrgData: string[] = stringData.map((object) => object.course.title);
+let newlabelData: string[] = [];
+avrgData.forEach((object: any[]) => {
+  
+  object.map((score) => {
+    console.log(score.avgScore);
+    newlabelData.push(score.text);
+    
+  });
+});
+
+let newScoreData: number[] = [];
+avrgData.forEach((object: any[]) => {
+  
+  object.map((score) => {
+    console.log(score.avgScore);
+    newScoreData.push(score.avgScore);
+    
+  });
+});
+// console.log(avrgDataTitle);
+console.log('avrgscore: ', newScoreData);
+
+
+const avrgScoreDiagramData = {
+	labels: newlabelData ,
+	datasets: [
+		{
+			label: "Course",
+			data: newScoreData,
+			backgroundColor: ['#00005C', '#3B185F', '#C060A1', '#F0CAA3', '#DC3535', '#FFE15D', '#FF8FB1'],
+			borderColor: '#3c2f2f'
+		}
+	],
+  options: {
+    responsive:true,
+    maintainAspectRatio: false
+    
+}
+}  
+  
+
+export { avrgScoreDiagramData}    
   
 /*     for (const categoryTypeName of categoryData) {
       categoryCount[categoryTypeName] = categoryCount[categoryTypeName]
@@ -18,7 +56,7 @@ console.log(data);
         : 1;
     }
     return categoryCount; */
-  }
+  
   
 /*   const categoryStats = avrgScore();
   const ByCategoryData = {
